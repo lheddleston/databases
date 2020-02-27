90ebb8c166f3f231ec653be79aae78f48821b2cb
lheddleston <laura.heddleston@gmail.com>
2020-02-26 17:41:22 -0800

    created chat database, set tables, created connection for mySql with express

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  room_id INT AUTO_INCREMENT PRIMARY Key,
+  room_name VARCHAR(30)
+);
+CREATE TABLE users (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_name VARCHAR(30)
+);
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id INT AUTO_INCREMENT PRIMARY KEY,
+  message_body VARCHAR(255),
+  user_id INT,
+  room_id INT,
+  FOREIGN KEY(user_id) REFERENCES users(user_id),
+  FOREIGN KEY(room_id) REFERENCES rooms(room_id)
 );
 
-/* Create other tables and define schemas for them here! */
+
 
 
 
